079edf5cae94c4518cf5554d5b866a94b43dda2b
Pelechi <khaingainnocent@yahoo.co.uk>
2023-03-23 04:42:00 -0700

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,8 @@
-#include "main.h"
-/**
- * positive_or_negative - checks for positive or negative numbers
- *
- * @i: The number to be checked
- *
- * Return: Always 0
- */
+ifndef MAIN_H
+#define MAIN_H
 
-void positive_or_negative(int i)
-{
-	if (i < 0)
-		printf("%d is negative", i);
-	else
-		printf("%d is positive", i);
-}
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
