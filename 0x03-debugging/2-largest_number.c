1e253e55cd481cfc64658fc645b9961a7d4afb55
Pelechi <khaingainnocent@yahoo.co.uk>
2023-03-24 06:45:26 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
@@ -28,6 +24,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
-return (0);
+	return (largest);
 }
