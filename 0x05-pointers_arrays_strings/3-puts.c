839f6da02899a6fd9f555d3a0b917b54e09bc274
Pelechi <khaingainnocent@yahoo.co.uk>
2023-03-28 11:27:57 -0700

    I can only go one way

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,14 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * Return: void
  */
 
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-	_putchar(*str++);
+	_putchar(*stri);
 	}
 	_putchar('\n');
 }
