e49ef58c7d7a896e027b1d52191d24102986afce
Pelechi <khaingainnocent@yahoo.co.uk>
2023-03-28 10:59:02 -0700

    swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two intergers
+ * @a: interger to swap
+ * @b: interger to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
